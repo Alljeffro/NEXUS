cacbb96eed5f419fe8b5c500a23ee83696ee68fd
Alljeffro <alfamarcellinsafidinirina@gmail.com>
2026-05-25 14:14:37 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css' // <-- Cette ligne doit être EXACTEMENT là pour charger Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
